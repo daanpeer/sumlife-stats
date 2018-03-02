ad8990d5a88e655653f158ea711b5a924f45aafc
Daan Houweling <daan.gdh@gmail.com>
2018-03-02 18:50:54 +0100

    Fixed colors

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ import './App.css';
 
 const answerColors = {
   '-1': '#4bcffa',
-  '0': '#ff3f34', // @todo change this color
-  '1': '#ff3f34',
+  '0': '#ff3f34',
+  '1': '#ff5e57',
   '2': '#ffa801',
   '3': '#ffd32a',
   '4': '#05c46b',
